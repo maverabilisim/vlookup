2f23450e7a5760ca872d9e083e9ab6242ccf9ccd
maverabilisim <maverabilisim@gmail.com>
2021-06-02 00:10:11 +0300

    Updates

---
+++ precache-manifest.12af87868e8f42cbf387043a6484d8f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7241bf2e8c20aaffbb2c4396552f5278",
+    "url": "/GolfScorecard/index.html"
+  },
+  {
+    "revision": "aab2e9293c5d67ae03b6",
+    "url": "/GolfScorecard/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "94a738eb5ddb79936b82",
+    "url": "/GolfScorecard/static/css/main.77cb0ec1.chunk.css"
+  },
+  {
+    "revision": "aab2e9293c5d67ae03b6",
+    "url": "/GolfScorecard/static/js/2.e677d639.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/GolfScorecard/static/js/2.e677d639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a738eb5ddb79936b82",
+    "url": "/GolfScorecard/static/js/main.c76e36f7.chunk.js"
+  },
+  {
+    "revision": "cbf75f75005c3a5cfb2d",
+    "url": "/GolfScorecard/static/js/runtime-main.62fe7318.js"
+  }
+]);
